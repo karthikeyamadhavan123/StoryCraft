2104a1e9e7c91e1e5137abebc9986592ee84b529
karthikeyamadhavan123 <karthikeyadonnipad2005@gmail.com>
2025-01-28 22:40:41 +0530

    five

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://storycraft-frontend.onrender.com',
     credentials: true,
   })
 );
